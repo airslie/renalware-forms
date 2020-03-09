c38a2f4445639324225be04afeec049ad199fe0c
Tim Crowe <tim@woodpigeon.com>
2020-03-09 10:28:55 +0000

    Correct allergies output
    When no_known_allergies is expliclty set, output "No Known Allergies"

--- spec/renalware/forms/homecare/generic/v1/allergies_spec.rb
+++ spec/renalware/forms/homecare/generic/v1/allergies_spec.rb
@@ -4,6 +4,18 @@ require "spec_helper"
 
 module Renalware::Forms::Generic
   RSpec.describe Homecare::V1::Allergies do
+    context "when the patient has iit recoreded that they have no known allergies" do
+      it do
+        args = Renalware::Forms::Homecare::Args.new(no_known_allergies: true)
+        doc = test_prawn_doc
+
+        described_class.new(doc, args).build
+
+        text = extract_text_from_prawn_doc(doc)
+        expect(text).to include("No Known Allergies")
+      end
+    end
+
     it do
       args = Renalware::Forms::Homecare::Args.new(allergies: %w(nuts penicillin))
       doc = test_prawn_doc
@@ -12,6 +24,8 @@ module Renalware::Forms::Generic
 
       text = extract_text_from_prawn_doc(doc)
 
+      expect(text).not_to include("No Known Allergies")
+      expect(text).to include("Known Allergies")
       expect(text).to include("nuts")
       expect(text).to include("penicillin")
     end
