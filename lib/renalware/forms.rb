2dc2cd0f0a3e6523ff64242d7ba2a152a94a22dc
Tim Crowe <tim@woodpigeon.com>
2020-11-15 18:58:07 +0000

    Add zeitwerk inflector

--- lib/renalware/forms.rb
+++ lib/renalware/forms.rb
@@ -5,14 +5,11 @@ require "prawn"
 require "prawn/table"
 require "attr_extras"
 
-# Note we don't use Zeitwerk::Loader.for_gem because we our root to be
-# the directly above this one (/lib/renalware) and for_gem uses __dir__ as the
-# root.
 loader = Zeitwerk::Loader.new
 loader.push_dir File.expand_path("..", __dir__)
 loader.ignore File.expand_path("../tasks", __dir__)
 loader.ignore File.expand_path("**/assets", __dir__)
-# loader.log!
+loader.inflector = Zeitwerk::GemInflector.new(File.join(__dir__, "forms"))
 loader.setup
 
 module Renalware
