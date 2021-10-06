8c2246c681ad2195b1c7c743e6febc2f16b64123
Tim Crowe <tim@woodpigeon.com>
2021-10-06 14:41:49 +0100

    Switch back to virtus from dry-struct
    The simplicity is preferable.

--- lib/renalware/forms/homecare/args.rb
+++ lib/renalware/forms/homecare/args.rb
@@ -1,68 +1,61 @@
 # frozen_string_literal: true
 
-require "dry-types"
-require "dry-struct"
+require "virtus"
 require "active_model"
 
 # rubocop:disable Metrics/ClassLength
 module Renalware::Forms
-  module Types
-    include Dry.Types()
-  end
-
-  class Homecare::Args < Dry::Struct
+  class Homecare::Args
+    include Virtus.model
     include ActiveModel::Validations
 
-    class Medication < Dry::Struct
+    class Medication
+      include Virtus.model(strict: true)
       include ActiveModel::Validations
 
-      attribute :date, Types::Params::Date
-      attribute :drug, Types::String
-      attribute :dose, Types::String
-      attribute :route, Types::String
-      attribute :frequency, Types::String
+      attribute :date, Date
+      attribute :drug, String
+      attribute :dose, String
+      attribute :route, String
+      attribute :frequency, String
     end
 
     # The first 2 attrbutes are used to drive what PDF to build
-    attribute :provider, Types::Coercible::String
-    attribute :version, Types::Coercible::Integer
-    attribute :title, Types::String.default("").optional
-    attribute :given_name, Types::String
-    attribute :family_name, Types::String
-    attribute :nhs_number, Types::String.default("").optional
-    attribute :born_on, Types::Params::Date
-    attribute :modality , Types::Coercible::String.optional
-    attribute :telephone, Types::String.default("").optional
-    attribute :hospital_number, Types::String.default("").optional
-    attribute :address, Types::Array.of(Types::Coercible::String).default([].dup, shared: true)
-    attribute :postcode, Types::String.default("").optional
-    attribute :prescriber_name, Types::String.default("").optional
-    # attribute :prescription_date, Types::Params::Date
-    attribute :hospital_name, Types::String.default("")
-    attribute :hospital_department, Types::String.default("").optional
-    attribute :hospital_address, Types::Array.of(Types::Coercible::String).default([], shared: true)
-    attribute :hospital_telephone, Types::String.default("").optional
-    attribute :po_number, Types::String.default("").optional
-    attribute :generated_at, (Types::Params::DateTime.default { DateTime.now })
-    attribute :no_known_allergies, Types::Bool.optional
-    attribute :allergies, Types::Array.of(Types::Coercible::String)
-    attribute :drug_type, Types::Coercible::String.optional
-    # attribute :prescription_duration, Types::String
-    attribute :administration_device, Types::String.default("").optional
-    attribute :medications, Types::Array.of(Medication).default([].dup, shared: true)
-    attribute :consultant, Types::String.default("").optional
-    attribute(
-      :delivery_frequencies,
-      Types::Array.of(Types::Coercible::String).default(["3 months", "6 months"].freeze)
-    )
-    attribute(
-      :prescription_durations,
-      Types::Array
-        .of(Types::Coercible::String)
-        .default(["3 months", "6 months", "12 months"].freeze)
-    )
-    attribute :selected_prescription_duration, Types::String.default("") # e.g. "6 months"
-    attribute :selected_delivery_frequency, Types::String.default("") # e.g. "6 months"
+    attribute :provider, String
+    attribute :version, Integer
+    attribute :title, String, default: ""
+    attribute :given_name, String
+    attribute :family_name, String
+    attribute :nhs_number, String, default: ""
+    attribute :born_on, Date
+    attribute :modality, String, default: ""
+    attribute :fmc_patient, String, default: ""
+    attribute :telephone, String, default: ""
+    attribute :hospital_number, String, default: ""
+    attribute :address, Array(String), default: []
+    attribute :postcode, String, default: ""
+    attribute :modality, String
+    attribute :prescriber_name, String, default: ""
+    attribute :prescription_date, Date
+    attribute :hospital_name, String
+    attribute :hospital_department, String
+    attribute :hospital_address, Array[String]
+    attribute :hospital_telephone, String
+    attribute :po_number, String
+    attribute :generated_at, DateTime
+    attribute :no_known_allergies, Boolean
+    attribute :allergies, Array[String]
+    attribute :drug_type, String
+    attribute :administration_route, String
+    attribute :administration_frequency, String
+    attribute :prescription_duration, String
+    attribute :administration_device, String
+    attribute :medications, Array[Medication]
+    attribute :consultant, String
+    attribute :delivery_frequencies, Array[String], default: ["3 months", "6 months"]
+    attribute :selected_delivery_frequency, String # e.g. "6 months"
+    attribute :prescription_durations, Array[String] # e.g. ["3 months", "6 months"]
+    attribute :selected_prescription_duration, String # e.g. "3 months"
 
     validates :family_name, presence: true
     validates :given_name, presence: true
@@ -94,7 +87,7 @@ module Renalware::Forms
     end
 
     def format_address_array(add)
-      return unless add&.is_a?(Array)
+      return unless add.is_a?(Array)
 
       add.compact.reject { |line| line == "" }.uniq&.join("\n")
     end
@@ -109,60 +102,64 @@ module Renalware::Forms
       Array(allergies).uniq.compact.join("<br>")
     end
 
+    # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
     def self.test_data(provider: :generic, version: 1)
-      args = new(
-        provider: provider,
-        version: version,
-        title: "Mr",
-        given_name: "Jack",
-        family_name: "JONES",
-        nhs_number: "0123456789",
-        born_on: Date.parse("2001-01-01"),
-        telephone: "07000 000001",
-        hospital_number: "ABC123",
-        modality: "HD",
-        address: ["line1", "", nil, "line2", "line3   "],
-        postcode: "TW1 1UU",
-        prescriber_name: "Dr X Yz",
-        prescription_date: Date.today.to_s,
-        consultant: "Dr Pepper",
-        hospital_name: "THE ROYAL LONDON HOSPITAL",
-        hospital_telephone: "0000 000001",
-        hospital_department: "Renal",
-        hospital_address: [
-          "The Royal London Hospital",
-          "Barts Health NHS Trust",
-          "Whitechapel",
+      new.tap do |args|
+        args.provider = provider
+        args.version = version
+        args.title = "Mr"
+        args.given_name = "Jack"
+        args.family_name = "JONES"
+        args.nhs_number = "0123456789"
+        args.born_on = Date.parse("2001-01-01")
+        args.fmc_patient = "123"
+        args.telephone = "07000 000001"
+        args.hospital_number = "ABC123"
+        args.modality = "PD"
+        args.address = ["line1", "", nil, "line2", "line3   "]
+        args.postcode = "TW1 1UU"
+        args.modality = "HD"
+        args.prescriber_name = "Dr X Yz"
+        args.prescription_date = Date.today.to_s
+        args.consultant = "Dr Pepper"
+        args.hospital_name = "THE ROYAL LONDON HOSPITAL"
+        args.hospital_telephone = "0000 000001"
+        args.hospital_department = "Renal"
+        args.hospital_address = [
+          nil,
+          "WHITECHAPEL",
+          "",
           "LONDON",
           "E1 1FR",
           "UK",
           "Tel: 0800 00000000",
           "Another line"
-        ],
-        no_known_allergies: false,
-        allergies: ["Nuts", nil, "Penicillin", "Mown grass"],
-        drug_type: "ESA",
-        prescription_duration: "1 month",
-        administration_device: "device?",
-        po_number: "P123",
-        delivery_frequencies: ["1 week", "3 months", "6 months", "12 month"],
-        prescription_durations: ["3 months", "6 months", "12 months"],
-        selected_prescription_duration: "6 months",
-        selected_delivery_frequency: "6 months"
-      )
+        ]
+        args.no_known_allergies = false
+        args.allergies = ["Nuts", nil, "Penicillin", "Mown grass"]
+        args.drug_type = "ESA"
+        args.administration_frequency = "Daily"
+        args.administration_route = "Per Oral"
+        args.prescription_duration = "1 month"
+        args.administration_device = "device?"
+        args.po_number = "P123"
+        args.generated_at = Time.now
+        args.delivery_frequencies = ["1 week", "3 months", "6 months", "12 month"]
+        args.prescription_durations = ["3 months", "6 months", "12 months"]
+        args.selected_prescription_duration = "6 months"
 
-      args.medications << Medication.new(
-        date: Date.today,
-        drug: "Example drug",
-        dose: "1 unit",
-        route: "PO",
-        frequency: "3"
-      )
+        args.medications << Medication.new(
+          date: Date.today,
+          drug: "Example drug",
+          dose: "1 unit",
+          route: "PO",
+          frequency: "3"
+        )
 
-      raise ArgumentError, args.errors unless args.valid?
-
-      args
+        raise ArgumentError, args.errors unless args.valid?
+      end
     end
+    # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 
     def medications_are_present
       if medications.size == 0
